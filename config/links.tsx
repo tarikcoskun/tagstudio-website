ea2e5348d7e500d068a93ff5b9163b90dde17bed
Tarık Coşkun <tarikcskun@gmail.com>
2024-10-10 17:57:06 +0300

    add portable download links

--- config/links.tsx
+++ config/links.tsx
@@ -5,16 +5,24 @@ import { Windows } from "@/components/Icon/standalone/Windows";
 export type Platforms = "Linux" | "Mac" | "Windows";
 
 export const downloads = {
-  Linux: {
+  "Windows": {
+    link: "https://github.com/TagStudioDev/TagStudio/releases/latest/download/tagstudio_windows_x86_64.zip",
+    icon: <Windows />,
+  },
+  "Windows (portable)": {
+    link: "https://github.com/TagStudioDev/TagStudio/releases/latest/download/tagstudio_windows_x86_64_portable.zip",
+    icon: <Windows />,
+  },
+  "Linux": {
     link: "https://github.com/TagStudioDev/TagStudio/releases/latest/download/tagstudio_linux_x86_64.tar.gz",
     icon: <Icon lib="lucide" icon="download" />,
   },
-  Mac: {
+  "Linux (portable)": {
+    link: "https://github.com/TagStudioDev/TagStudio/releases/latest/download/tagstudio_linux_x86_64_portable.tar.gz",
+    icon: <Icon lib="lucide" icon="download" />,
+  },
+  "Mac": {
     link: "https://github.com/TagStudioDev/TagStudio/releases/latest/download/tagstudio_macos_x86_64.tar.gz",
     icon: <Apple />,
-  },
-  Windows: {
-    link: "https://github.com/TagStudioDev/TagStudio/releases/latest/download/tagstudio_windows_x86_64.zip",
-    icon: <Windows />,
   },
 };

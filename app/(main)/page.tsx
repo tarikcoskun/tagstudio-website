041138d5885b4f23038da8c79337f22b925ae479
Tarık Coşkun <tarikcskun@gmail.com>
2024-10-10 17:58:30 +0300

    redundant code

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,15 +4,9 @@ import { Showcase } from "@/ui/Showcase";
 import { Features } from "@/ui/Features";
 import { CTA } from "@/ui/CTA";
 
-// Styles
-import style from "./page.module.scss";
-import classNames from "classnames/bind";
-
-const cx = classNames.bind(style);
-
 export default function HomePage() {
   return (
-    <main className={cx("page")}>
+    <main>
       <Hero />
       {/* <Showcase /> */}
       <Features />

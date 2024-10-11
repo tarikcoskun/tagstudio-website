22fa2a8fa3d8785a8490d2ac553170f74da1e267
Tarık Coşkun <tarikcskun@gmail.com>
2024-10-11 21:33:50 +0300

    show hidden animated elements when js is disabled

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,6 +5,13 @@ import { Footer } from "@/components/layout/Footer";
 export default function MainLayout({ children }: React.PropsWithChildren) {
   return (
     <>
+      <noscript>
+        <style dangerouslySetInnerHTML={{
+          __html: "[style*=\"opacity:0\"] { opacity: unset !important; transform: unset !important; }",
+        }}
+        />
+      </noscript>
+
       <Navbar />
       {children}
       <Footer />

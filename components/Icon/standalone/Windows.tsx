8c3b3d193ecd7b342a3017a581131eb894e6433e
Tarık Coşkun <tarikcskun@gmail.com>
2024-11-10 14:41:03 +0300

    update windows icon

--- components/Icon/standalone/Windows.tsx
+++ components/Icon/standalone/Windows.tsx
@@ -1,7 +1,7 @@
 export function Windows(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 24 24" {...props}>
-      <path fill="currentColor" d="M22 2L11.2 3.6v8l10.8-.1zM10.2 12.5L2 12.4v6.8l8.1 1.1zM2 4.8v6.8h8.1V3.7zm9.1 7.7v7.9L22 22v-9.4z"></path>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 16" width="16px" height="15.05" {...props}>
+      <path fill="currentColor" d="M7.60378 7.60256H0.0012207V0H7.60378V7.60256ZM15.9987 7.60256H8.39546V0H15.998V7.60256H15.9987ZM7.60378 16H0.0012207V8.39744H7.60378V16ZM15.9987 16H8.39546V8.39744H15.998V16H15.9987Z"></path>
     </svg>
   );
 }
